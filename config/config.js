1ef17c8ab102c46b38a5f32e7c3db9697537b0d8
44fwvnh55b-alt <44fwvnh55b@privaterelay.appleid.com>
2026-01-03 15:03:19 +0100

    Update bot configuration values
    Updated token, botId, and buyers list in config.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  token: "Put-Token-Here",
-  botId: "Bot-Id",
-  buyers: ["276705058370879489"],
+  token: "MTQ1NjQ5ODgyMTU3MDc2MDc4OA.GwL22g.dpL5sLNZ_hZdMPQu7HmzU65Bb25DS5xNcnLqvs",
+  botId: "1456498821570760788",
+  buyers: ["1382295703778496542"],
   prefix: "+",
   color: "#2b2d31",
 };
